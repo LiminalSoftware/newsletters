b19be216e5d002fccf33d98208d1c78f4a9181cb
Bryan White <bryanchriswhite@gmail.com>
2016-05-04 16:41:15 +0200

    preparing for redux

---
+++ components/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import StickyHeader from './stickyHeader';
+import DefaultTemplate from '../templates/default';
+
+const App = () => {
+    return (
+      <div>
+        <StickyHeader rootSelector="#content-container"/>
+        <DefaultTemplate />
+      </div>
+    )
+};
+
+export default App;
--- index.jsx
+++ index.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import StickyHeader from './components/stickyHeader.jsx';
-import DefaultTemplate from './templates/default/index.jsx';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
+import App from './components/App';
 
-export class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <StickyHeader rootSelector="#content-container"/>
-        <DefaultTemplate />
-      </div>
-    );
-  }
-}
+const store = createStore(reducer);
 
-ReactDOM.render(<App/>, document.querySelector("#editor"));
+render(
+    <App/>,
+  document.getElementById("root")
+);
